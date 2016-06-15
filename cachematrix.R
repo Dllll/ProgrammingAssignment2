701fe693c4c4dd98750c028b8fc5f9529bdd4b85
Dllll <londonodaniela@gmail.com>
2016-06-14 21:45:15 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,31 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#This function create a matrix object
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv<-NULL
+	set<-function(y){
+		x<<-y
+		inv<<-NULL
+	}
+	get<-function()x
+	setInverse<-function(inverse)inv
+	getInverse<-function()inv
+	list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
-
+#This function computes the inverse of the matrix that is above
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv<-x$getInverse()
+        if (!is.NULL(inv)){
+        	message("getting data")
+        	return(inv)
+        }
+        mat<-x$get()
+        inv<-solve(mat,...)
+        x$setInverse(inv)
+        inv
 }
